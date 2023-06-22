2b79d8e186951c6c220e7932e434214e2f161c13
Rebecca D <rdolmon08@gmail.com>
2023-06-22 10:21:13 -0500

    css and js in progress

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeColor(){
+var hex_numbers = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]; 
+
+var hexcode = '';
+
+for(var i=0;i < 6; i++){
+    var random_index = Math.floor(Math.random() * hex_numbers.length); 
+}
+}
+
+//https://www.youtube.com/watch?v=4nCH8EA55js&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=2&ab_channel=WebDevTutorials
