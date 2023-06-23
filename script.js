2723b58d109b211a07b7dcaa6ed3ed01ed9f9d6d
Rebecca D <rdolmon08@gmail.com>
2023-06-23 08:13:00 -0500

    finished js

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ var hexcode = '';
 
 for(var i=0;i < 6; i++){
     var random_index = Math.floor(Math.random() * hex_numbers.length); 
+
+    hexcode += hex_numbers[random_index]
 }
+
+document.getElementById("hex-code").innerHTML = hexcode; 
+document.getElementsByTagName("body")[0].style.background = "#" + hexcode; 
+
 }
 
 //https://www.youtube.com/watch?v=4nCH8EA55js&list=PLtMugc7g4GaqAVDZwQ_t1H6500ZGJzOgW&index=2&ab_channel=WebDevTutorials
